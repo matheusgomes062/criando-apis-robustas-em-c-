bcbc30045b640b405ffd17d962e31d9ec024776a
Matheus Gomes <matheusgomes062@gmail.com>
2022-02-13 19:06:00 -0300

    feat: Implementando nosso repository

---
+++ XGame/XGame.Infra/Persistence/Repositories/RepositoryJogador.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using XGame.Domain.Entities;
+using XGame_Domain.Interfaces.Repositories;
+
+namespace XGame.Infra.Persistence.Repositories
+{
+    public class RepositoryJogador : IRepositoryJogador
+    {
+        protected readonly XGameContext _context;
+
+        public RepositoryJogador(XGameContext context)
+        {
+            _context = context;
+        }
+
+        public Jogador AdicionarJogador(Jogador jogador)
+        {
+            _context.Jogadores.Add(jogador);
+            _context.SaveChanges();
+            return jogador;
+        }
+
+        public void AlterarJogador(Jogador jogador)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Jogador AutenticarJogador(string email, string senha)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Jogador> ListarJogador()
+        {
+            return _context.Jogadores.ToList();
+        }
+
+        public Jogador ObterJogadorPorId(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
